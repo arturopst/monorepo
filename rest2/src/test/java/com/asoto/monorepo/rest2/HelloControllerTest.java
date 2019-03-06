5f3645b0b6a18c6367f933e0570ea1f301ceb9a1
Arturo Soto <arturo.soto@outlook.com>
2019-03-06 09:36:46 -0700

    adding a test

---
+++ rest2/src/test/java/com/asoto/monorepo/rest2/HelloControllerTest.java
@@ -0,0 +1,15 @@
+package com.asoto.monorepo.rest2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloControllerTest {
+
+    @Test
+    public void alwayPassTest(){
+
+        assertTrue(true == true);
+    }
+
+}
